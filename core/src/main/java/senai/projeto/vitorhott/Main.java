1ae8d52dcc2befaca99069f9a5a7a82d8b22ce57
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-11-06 19:55:14 -0300

    06.11.2025

--- core/src/main/java/senai/projeto/vitorhott/Main.java
+++ core/src/main/java/senai/projeto/vitorhott/Main.java
@@ -10,7 +10,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -32,6 +34,13 @@ public class Main extends ApplicationAdapter {
 
     Vector2 touchPos;
 
+    Array<Sprite> dropSprites;
+
+    float dropTime;
+
+    Rectangle bucketRentangle;
+    Rectangle dropRectangle;
+
     @Override
     public void create() {
         // Inicializa os recursos
@@ -52,6 +61,15 @@ public class Main extends ApplicationAdapter {
         buckerSprite.setSize( 1, 1);
 
         touchPos = new Vector2();
+
+        dropSprites = new Array<>();
+
+        bucketRentangle = new Rectangle();
+        dropRectangle = new Rectangle();
+
+        music.setLooping(true);
+        music.setVolume(.5f);
+        music.play();
     }
 
     @Override
@@ -90,7 +108,39 @@ public class Main extends ApplicationAdapter {
         float worldWidht = viewport.getWorldHeight();
         float  worldHeight = viewport.getWorldHeight();
 
-        buckerSprite.set(MathUtils.clamp(buckerSprite.getX(),0, worldWidht);
+        float bucketWidth = bucketTexture.getWidth();
+        float bucketHeigt = buckerSprite.getHeight();
+
+        buckerSprite.setX(MathUtils.clamp(buckerSprite.getX(),0, worldWidht - bucketHeigt));
+
+        float delta  =  Gdx.graphics.getDeltaTime();
+
+        bucketRentangle.set(buckerSprite.getX(), buckerSprite.getY(), bucketWidth,bucketHeigt);
+
+        for(int i = dropSprites.size - 1; i >= 0; i--){
+            Sprite dropeSprite =  dropSprites.get(i);
+            float dropWidth = dropeSprite.getWidth();
+            float  dropHeight = dropeSprite.getHeight();
+
+
+            dropeSprite.translateY((-2 * delta));
+            dropRectangle.set(dropeSprite.getX(), dropeSprite.getY(), dropWidth, dropHeight);
+
+            if (dropeSprite.getY() < - dropHeight) dropSprites.removeIndex(i);
+            else if (bucketRentangle.overlaps(dropRectangle)){
+                dropSprites.removeIndex(i);
+                dropSound.play();
+            }
+
+
+        }
+
+        dropTime += delta;
+        if (dropTime > 1f) {
+            dropTime = 0;
+            cretaDroplet();
+        }
+
     }
 
     private void draw() {
@@ -104,10 +154,27 @@ public class Main extends ApplicationAdapter {
 
 
         spriteBatch.draw(backgroundTexture, 0, 0, worldWidth,worldHeight);
-        buckerSprite.draw(spriteBatch);
+        spriteBatch.draw(bucketTexture,0, 0, 1, 1);
 
+        for(Sprite dropSprite: dropSprites){
+            dropSprite.draw(spriteBatch);
+        }
 
         spriteBatch.end();
+
+    }
+    private  void cretaDroplet(){
+        float dropWidht = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(dropWidht, dropHeight);
+        dropSprite.setX(MathUtils.random(0f, worldWidth - worldHeight));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
     }
 
     @Override
